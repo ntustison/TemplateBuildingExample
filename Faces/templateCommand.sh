f12682aa3fa0dc82b368cdef6813275eac829263
ntustison <ntustison@gmail.com>
2013-10-29 20:25:46 -0400

    Initial commit.

---
+++ Faces/templateCommand.sh
@@ -0,0 +1,22 @@
+ANTsPath=/Users/ntustison/Pkg/ANTs/bin/bin/
+inputPath=${PWD}/
+
+
+${ANTsPath}/antsMultivariateTemplateConstruction2.sh \
+  -d 2 \
+  -o ${inputPath}T_ \
+  -i 4 \
+  -g 0.25 \
+  -j 2 \
+  -c 0 \
+  -k 1 \
+  -w 1 \
+  -f 16x12x8x4x2x1 \
+  -s 4x4x4x2x1x0 \
+  -q 100x100x100x70x50x10 \
+  -n 0 \
+  -r 0 \
+  -m CC \
+  -t BSplineSyN[0.1,75,0] \
+  ${inputPath}/*.nii.gz
+
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 TemplateBuildingExample
 =======================
 
-I'll tell you where. Someplace warm. A place where the beer flows like wine. Where beautiful women instinctively flock like the salmon of Capistrano. I'm talking about a little place called Aspen.
