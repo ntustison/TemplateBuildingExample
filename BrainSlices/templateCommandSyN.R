b054a0e42b5ea5acd0aca80ee12a6ae1482be1ce
Nick Tustison <ntustison@gmail.com>
2019-05-08 14:15:30 -0700

    ENH:  Add ANTsR analog.

--- BrainSlices/templateCommandSyN.R
+++ BrainSlices/templateCommandSyN.R
@@ -19,20 +19,3 @@ btp <- buildTemplate( initialTemplate = averageImage,
   synMetric = 'CC',
   regIterations = c( 100, 70, 50, 10 ) )
 
-
-
-${ANTSPATH}/antsMultivariateTemplateConstruction.sh \
-  -d 2 \
-  -o ${outputPath}T_ \
-  -i 4 \
-  -g 0.2 \
-  -j 4 \
-  -c 2 \
-  -k 1 \
-  -w 1 \
-  -m 100x70x50x10 \
-  -n 1 \
-  -r 1 \
-  -s CC \
-  -t GR \
-  ${inputPath}/OASIS*1.nii.gz
